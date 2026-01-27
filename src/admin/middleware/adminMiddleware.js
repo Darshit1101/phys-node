ba8170729ed56425b03d15199a804b4f951a3bc9
Darshit Kakadiya <darshit.kakadiya@shunyavkash.com>
2026-01-27 16:59:44 +0530

    feat(auth): implement adminMiddleware for user authentication and remove authMiddleware

---
+++ src/admin/middleware/adminMiddleware.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+import { APP_JWT_SECRET } from "../../configs/environment.js";
+import { Cookie } from "../../constants/Cookies.js";
+
+const adminMiddleware = (req, res, next) => {
+  try {
+    const token = req.cookies?.[Cookie.ADMIN_TOKEN];
+
+    if (!token) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const decoded = jwt.verify(token, APP_JWT_SECRET);
+    console.log('decoded token===>', decoded);
+
+    req.user = {
+      id: decoded.id,
+    };
+
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid or expired token" });
+  }
+};
+
+export default adminMiddleware;
--- src/admin/routes/v1/user.js
+++ src/admin/routes/v1/user.js
@@ -1,12 +1,11 @@
 import { Router } from 'express';
 import getAllUsers from '../../controllers/v1/user/getAllUsers.js';
-import authMiddleware from '../../../middleware/authMiddleware.js';
-import { Cookie } from '../../../constants/Cookies.js';
+import adminMiddleware from '../../middleware/adminMiddleware.js';
 
 const router = Router();
 router.post(
   '/getAllUsers',
-  authMiddleware([Cookie.ADMIN_TOKEN, Cookie.AUTH_TOKEN]),
+  adminMiddleware,
   getAllUsers
 );
 
