4167497912e8027df47851ba72e2e9d94f179956
Darshit Kakadiya <darshit.kakadiya@shunyavkash.com>
2026-01-27 12:08:41 +0530

    fix(user): update allowRoles to use Role constant for getAllUsers endpoint

--- src/admin/routes/v1/user.js
+++ src/admin/routes/v1/user.js
@@ -2,8 +2,9 @@ import { Router } from 'express';
 import getAllUsers from '../../controllers/v1/user/getAllUsers.js';
 import authMiddleware from '../../../middleware/authMiddleware.js';
 import { allowRoles } from '../../../middleware/roleMiddleware.js';
+import { Role } from '../../../constants/Role.js';
 
 const router = Router();
-router.post('/getAllUsers', authMiddleware, allowRoles("admin"), getAllUsers);
+router.post('/getAllUsers', authMiddleware, allowRoles(Role.ADMIN), getAllUsers);
 
 export default router;
